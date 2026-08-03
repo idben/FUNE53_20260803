05d88211cc9541e0f87ddddd235ec011a5bbbffa
Ben Chen <idben.chen@gmail.com>
2026-08-03 11:15:11 +0800

    ws斷開連線

--- websocket1/index.js
+++ websocket1/index.js
@@ -1,7 +1,13 @@
 import WebSocket, { WebSocketServer } from "ws";
+// 終端機指令: npm run dev 或 node index.js
+// .js 檔有更新，終端機要斷開再重新執行
 
 const wss = new WebSocketServer({ port: 8080 });
-wss.on("connection", () => {
-    console.log("新的使用者已連線");
+wss.on("connection", (connection) => {
+    console.log("新的使用者已連線了哦~~");
 
+
+    connection.on("close", () => {
+        console.log("使用者已斷開連線");
+    });
 });
