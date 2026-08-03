5b2a5d4a6d79a3af36324df761922920471fda96
Ben Chen <idben.chen@gmail.com>
2026-08-03 10:56:50 +0800

    簡單的 websocket server 與簡單的連線

--- websocket1/index.js
+++ websocket1/index.js
@@ -1,1 +1,7 @@
-console.log("QQ");
+import WebSocket, { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", () => {
+    console.log("新的使用者已連線");
+
+});
